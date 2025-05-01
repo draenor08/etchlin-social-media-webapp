457375c17e0d01b5647caf7dde0bcd2d4fdf4fa7
draenor08 <khondokarsazid08@gmail.com>
2025-05-02 00:21:16 +0600

    Some frontend functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,10 @@
-import React from "react";
-import GlobalStyles from "./styles/GlobalStyles";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import { UserProvider } from "./context/UserContext";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
 
 function App() {
-  return (
-    <GlobalStyles>
-      <div style={{color:"black", background: "white"}}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </Router>
-      </div>
-    </GlobalStyles>
-  );
+  return <Profile/>;
 }
 
 export default App;
---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,40 @@
+import "./profile.css";
+import Topbar from "../components/Topbar";
+import Sidebar from "../components/Sidebar";
+import Feed from "../components/Feed";
+import Rightbar from "../components/RIghtbar";
+
+export default function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/7.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                <span className="profileInfoDesc">Hello my friends!</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
