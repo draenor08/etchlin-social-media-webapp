1b06ed31491ec5d58f96da3e6c0a5b700a2b94f6
draenor08 <khondokarsazid08@gmail.com>
2025-05-06 00:20:37 +0600

    Forgot to add frontend host url :3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,4 +87,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'\
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", #frontend-connection
+]
 
